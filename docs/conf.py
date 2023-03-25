062f6b154773c65b042904befc341a17916da78e
mtakagishi <mtakagishi+github@gimail.com>
2023-03-25 21:55:03 +0900

    sitemapを修正。日本語モードoff

--- docs/conf.py
+++ docs/conf.py
@@ -33,7 +33,7 @@ html_last_updated_fmt = '%Y/%m/%d'
 extensions = [
     'sphinx.ext.todo',
     'recommonmark',
-    # 'sphinx_sitemap',
+    'sphinx_sitemap',
     'sphinxnotes.strike',
     'sphinxcontrib.blockdiag',
 ]
@@ -46,7 +46,7 @@ templates_path = ['_templates']
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = 'ja'
+# language = 'ja'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
@@ -89,9 +89,9 @@ source_suffix = {
     '.md': 'markdown',
 }
 # sphinx-sitemap
-# html_baseurl = 'https://yurugengo.mtakagishi.com/'
-# html_extra_path = ['robots.txt']
-# sitemap_locales = [None]
+html_baseurl = 'https://yurugengo.mtakagishi.com/'
+sitemap_locales = [None]
+html_extra_path = ['robots.txt']
 
 # blockdiag
 blockdiag_html_image_format = 'SVG'
