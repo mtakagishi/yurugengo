0d86d2bd2edda97d4f3e0258d90aff4566813c6a
mtakagishi <mtakagishi+github@gimail.com>
2025-02-10 14:08:06 +0900

    広告調整

--- docs/conf.py
+++ docs/conf.py
@@ -80,9 +80,9 @@ html_theme_options["analytics"] = {
     "google_analytics_id": "G-ZQ74H2ZF8F",
 }
 html_sidebars = {
-    "words/*": ["search-field.html","ads_google.html"],
-    "reference/*": ["search-field.html","ads_google.html"],
-    "videos/timeline": ["ads_google.html"],
+    # "words/*": ["search-field.html","ads_google.html"],
+    # "reference/*": ["search-field.html","ads_google.html"],
+    # "videos/timeline": ["ads_google.html"],
     # "**": ["search-field.html", "ads_top_medium_rectangle.html"],
     "**": ["search-field.html", "ads_google.html"],
 }
